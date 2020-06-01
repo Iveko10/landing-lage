72a6d4f0c56b785f8dea5aebbf4a1047f190ad83
Ivan Sefcik <isefcik@centrum.sk>
2020-06-01 16:52:47 +0200

    Updates

---
+++ precache-manifest.2c9f8e9a8e8022ca887c6416dc56e184.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68b9fed3f4aee06cd6247c4577d5404",
+    "url": "/index.html"
+  },
+  {
+    "revision": "964fb3522955e0131b42",
+    "url": "/static/css/main.714cc4ae.chunk.css"
+  },
+  {
+    "revision": "e87ae84534fda8c93075",
+    "url": "/static/js/2.bc9a42ed.chunk.js"
+  },
+  {
+    "revision": "0795701ba7555fb0a8a6d3507fdf4a12",
+    "url": "/static/js/2.bc9a42ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964fb3522955e0131b42",
+    "url": "/static/js/main.15fa9c41.chunk.js"
+  },
+  {
+    "revision": "d6583506a5e8e2338d62",
+    "url": "/static/js/runtime-main.b778f60f.js"
+  },
+  {
+    "revision": "d9cca9d6b4b2a8dcb50eea92bf479e9a",
+    "url": "/static/media/d.d9cca9d6.jpg"
+  },
+  {
+    "revision": "b1cf28eee023cc4e970c70a5cf41df52",
+    "url": "/static/media/invoices-1b.b1cf28ee.png"
+  }
+]);
